c3320cf34f767f5ce921214a7299ff10b91bbae6
lumosity4tpj <46916880+lumosity4tpj@users.noreply.github.com>
2019-07-24 19:50:31 +0800

    Add files via upload

--- csv2json_code/transfer_csv2json.sh
+++ csv2json_code/transfer_csv2json.sh
@@ -2,19 +2,19 @@
 
 . ./CONFIG
 
-python3 csv2json.py \
-    --image_dir ${TRAIN_IMAGE_DIR} \
-    --categories_csv_path ${CATEGORIES_CSV_PATH} \
-    --categories_json_path ${CATEGORIES_JSON_PATH} \
-    --annotation_csv_path ${ANNOTATION_CSV_PATH} \
-    --json_save_path ${TRAIN_JSON_SAVE_PATH} \
+# python3 csv2json.py \
+#     --image_dir ${TRAIN_IMAGE_DIR} \
+#     --categories_csv_path ${CATEGORIES_CSV_PATH} \
+#     --categories_json_path ${CATEGORIES_JSON_PATH} \
+#     --annotation_csv_path ${TRAIN_ANNOTATION_CSV_PATH} \
+#     --json_save_path ${TRAIN_JSON_SAVE_PATH} \
 
 
 python3 csv2json.py \
     --image_dir ${VAL_IMAGE_DIR} \
     --categories_csv_path ${CATEGORIES_CSV_PATH} \
     --categories_json_path ${CATEGORIES_JSON_PATH} \
-    --annotation_csv_path ${ANNOTATION_CSV_PATH} \
+    --annotation_csv_path ${VAL_ANNOTATION_CSV_PATH} \
     --json_save_path ${VAL_JSON_SAVE_PATH} \
 
 
@@ -22,5 +22,4 @@ python3 csv2json.py \
     --image_dir ${TEST_IMAGE_DIR} \
     --categories_csv_path ${CATEGORIES_CSV_PATH} \
     --categories_json_path ${CATEGORIES_JSON_PATH} \
-    --annotation_csv_path ${ANNOTATION_CSV_PATH} \
     --json_save_path ${TEST_JSON_SAVE_PATH} \
